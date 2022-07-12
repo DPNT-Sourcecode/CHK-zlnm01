5cfc45f1f9b38901f4f2ad9fc9ac579c0ed36878
sbx_user1051 <sbx_user1051@169.254.6.253>
2022-07-12 20:48:45 +0000

    Tue Jul 12 20:48:45 UTC 2022

--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -1,6 +1,7 @@
-from .checkout_solution import checkout
+from checkout_solution import checkout
 
 
+# CHK_R1 test
 assert checkout("A") == 50
 assert checkout("B") == 30
 assert checkout("C") == 20
@@ -11,3 +12,9 @@ assert checkout("BBBBB") == 120
 
 
 assert checkout("DABCABCABC") == 130 + 45 + 30 + 20 * 3 + 15
+
+assert checkout("DEF") == -1
+assert checkout("a") == -1
+
+# CHK_R2 test
+
