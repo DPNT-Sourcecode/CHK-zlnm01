340aefc8d2225f815df2b6b8285439e825e307ac
sbx_user1051 <sbx_user1051@169.254.6.253>
2022-07-12 20:42:36 +0000

    Tue Jul 12 20:42:36 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,11 +24,3 @@ def checkout(skus):
     return total
 
 
-
-assert checkout("A") == 50
-assert checkout("B") == 30
-assert checkout("C") == 20
-assert checkout("D") == 15
-
-assert checkout("A") == 50
-
--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -1,8 +1,14 @@
 from .checkout_solution import checkout
 
+
 assert checkout("A") == 50
 assert checkout("B") == 30
 assert checkout("C") == 20
 assert checkout("D") == 15
 
-assert checkout("A") == 50
+assert checkout("AAAA") == 180
+assert checkout("BBBBB") == 120
+
+
+assert checkout("DABCABCABC") == 130 + 45 + 30 + 20 * 3 + 15
+
