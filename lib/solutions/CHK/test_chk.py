e3191dd32513fea931191b323515adcc8fb305de
sbx_user1051 <sbx_user1051@169.254.7.125>
2022-07-13 06:55:58 +0000

    Wed Jul 13 06:55:58 UTC 2022

--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -1,37 +1,40 @@
+import unittest
+
+from parameterized import parameterized
+
 from checkout_solution import checkout
 
 
-# CHK_R1 test
-assert checkout("A") == 50
-assert checkout("B") == 30
-assert checkout("C") == 20
-assert checkout("D") == 15
+class Test(unittest.TestCase):
+    @parameterized.expand(
+        [
+            # CHK_R1 test
+            ("A", 50),
+            ("B", 30),
+            ("C", 20),
+            ("D", 15),
+            ("AAAA", 180),
+            ("BBBBB", 120),
+            ("DABCABCABC", 130 + 45 + 30 + 20 * 3 + 15),
+            # Invalid
+            ("DEF", -1),
+            ("a", -1),
+            # CHK_R2 test
+            # EEB asserts
+            ("EE", 80),
+            ("EEB", 80),
+            ("BEE", 80),
+            ("EBE", 80),
+            ("EBEEBBEB", 160 + 45),
+            # Multi A asserts
+            ("AAAAA", 200),
+            ("AAAAAAAA", 330),
+            ("AAAAAAAAA", 380),
+            ("AAAAAAAAAA", 400),
+            # cheaper than 5 + 2 * 3, assume rule generally holds so don't need to check all combos of discounts
+            ("AAAAAAAAAAA", 450),
+        ]
+    )
+    def test_checkout_price(self, input, expected):
+        self.assertEqual(checkout(input), expected)
 
-print(checkout("AAAA"))
-assert checkout("AAAA") == 180
-assert checkout("BBBBB") == 120
-
-
-assert checkout("DABCABCABC") == 130 + 45 + 30 + 20 * 3 + 15
-
-assert checkout("DEF") == -1
-assert checkout("a") == -1
-
-# CHK_R2 test
-
-# EEB asserts
-print(checkout("EEB"))
-assert checkout("EE") == 80
-assert checkout("EEB") == 80
-assert checkout("BEE") == 80
-assert checkout("EBE") == 80
-print(checkout("EBEEBBEB"))
-assert checkout("EBEEBBEB") == 160 + 45
-
-# Multi A asserts
-assert checkout("AAAAA") == 200
-print(checkout("AAAAAAAA"))
-assert checkout("AAAAAAAA") == 330
-assert checkout("AAAAAAAAA") == 380
-assert checkout("AAAAAAAAAA") == 400
-assert checkout("AAAAAAAAAAA") == 450 # cheaper than 5 + 2 * 3, assume rule generally holds so don't need to check all combos of discounts
